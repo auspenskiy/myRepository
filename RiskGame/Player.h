5a9a4febe7834e41a3148a2d32fbbada5ecbae88
andych92 <chenandy92@hotmail.com>
2015-03-31 17:11:10 -0400

    test commit

--- RiskGame/Player.h
+++ RiskGame/Player.h
@@ -1,8 +1,23 @@
 #pragma once
-class Player
-{
+#include <string>
+
+class Player{
 public:
-	Player();
+	Player(std::string aName);
 	~Player();
+	void setName(std::string);
+	std::string getName();
+	void setPlayerIndex(int index);
+	int getBattlesWon();
+	int getBattlesLost();
+	int getNumCountriesOwned();
+	int getNumArmiesOwned();
+private:
+	int battlesWon;
+	int battlesLost;
+	int playerIndex;
+	int numCountriesOwned;
+	int numArmiesOwned;
+	std::string name;
 };
 
